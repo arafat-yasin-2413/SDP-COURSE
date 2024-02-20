ff6f00dccb8c4c75dbb40791c316f7b594b40e91
arafat-yasin-2413 <129652012+arafat-yasin-2413@users.noreply.github.com>
2024-02-20 23:17:38 +0600

    Sixth Project Done

---
+++ WEEK-4/Moduel 14 Recap-2/sixth_project/first_app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from . import models
+
+
+# Create your views here.
+def home(request):
+    student = models.Student.objects.all()
+    # print(student)
+    return render(request, 'home.html', {'data':student})
+
+
+def delete_student(request,roll):
+    student = models.Student.objects.all()
+    std = models.Student.objects.get(pk=roll).delete()
+    print(std)
+    # return render(request, 'home.html', {'data':student})
+    return redirect('homepage')

6910e5fc6f9891ca49249be1189eb41f445531e2
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2024-07-10 09:22:02 +0600

    Module 21 Done

---
+++ WEEK-6/Module 21/mamar_bank/accounts/constants.py
@@ -0,0 +1,11 @@
+# tuples of tuple . prothom ta user er jonno, second ta backend er jonno
+
+ACCOUNT_TYPE= (
+    ('Savings', 'Savings'),
+    ('Current', 'Current'),
+)
+
+GENDER_TYPE= (
+    ('Male', 'Male'),
+    ('Female', 'Female'),
+)
